ca5a5f8e3381618b70c711274025afaa91191c81
zwartapps <zwartapps@gmail.com>
2018-12-02 12:47:23 +0000

    comments added

--- src/main/java/es/formacion/cip/Principal.java
+++ src/main/java/es/formacion/cip/Principal.java
@@ -4,15 +4,10 @@ import javax.swing.*;
 
 public class Principal {
 
+    //Metodo main para arrancar con el menu principal
     public static void main(String[] args) {
 
         VentanaPrincipal v = new VentanaPrincipal();
         v.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
-       /* c.conectar("192.168.1.38", "formacion", "formacion");
-        c.desconectar();
-*/
     }
-
 }
